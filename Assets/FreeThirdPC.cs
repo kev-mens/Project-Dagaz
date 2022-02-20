a47d4dcda06222b1e8fccd2fe8130c0ad396d3e7
Kevin Armah Mensah <54510561+kev-mens@users.noreply.github.com>
2022-02-19 21:53:23 -0500

    smoothed out rotations

--- Assets/FreeThirdPC.cs
+++ Assets/FreeThirdPC.cs
@@ -10,7 +10,7 @@ public class FreeThirdPC : MonoBehaviour
     Vector3 gravityDir = Vector3.zero;
     CharacterController controller;
     float gravity = 8f;
-
+    float rotateSpeed = 10f;
     
     // Start is called before the first frame update
     void Start()
@@ -23,47 +23,58 @@ public class FreeThirdPC : MonoBehaviour
         // W only
         if(Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y, Vector3.up);
+            //snaps character to given angle
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y, Vector3.up);
+
+            //smoothly rotate character to given angle
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y, Vector3.up), rotateSpeed);
         }
 
         // A only
         if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+270f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+270f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+270f, Vector3.up), rotateSpeed);
         }
 
         // S only
         if(Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+180f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+180f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+180f, Vector3.up), rotateSpeed);
         }
 
         // D only
         if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+90f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+90f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+90f, Vector3.up), rotateSpeed);
         }
 
         //W combinations
         if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 315f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 315f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+315f, Vector3.up), rotateSpeed);
         }
 
         if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 45f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 45f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+45f, Vector3.up), rotateSpeed);
         }
 
         //S combinations
         if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 225f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 225f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+225f, Vector3.up), rotateSpeed);
         }
 
         if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
         {
-            transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 135f, Vector3.up);
+            //transform.rotation = Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y + 135f, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(freeLookCam.transform.eulerAngles.y+135f, Vector3.up), rotateSpeed);
         }
     }
 
